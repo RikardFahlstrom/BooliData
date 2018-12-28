0af0500c119c70b3d8d38d53458877ef6f6713f1
Rikard Fahlstrom <rikard.fahlstrom@gmail.com>
2018-12-28 11:42:29 +0100

    updated requirements version after security alert. Also minor adjustment in program structure.

--- program.py
+++ program.py
@@ -12,6 +12,13 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
+def main():
+    output = create_api_request('API_CALLERID', 'API_KEY', '386690,870600')
+    df = create_df(output)
+    html = create_html(df)
+    send_email('rikard.fahlstrom@gmail.com', 'rikard.email.notification@gmail.com', 'EMAIL_PW', html)
+
+
 def create_api_request(api_callerid, api_key, area):
     global time, output
     callerId = api_callerid
@@ -22,10 +29,10 @@ def create_api_request(api_callerid, api_key, area):
     query_type = 'listings'
     areaId = area
     offset = '0'
-    limit = str(10)  # Maximum number of response objects. Limit = 500
+    limit = str(15)  # Maximum number of response objects. Limit = 500
     object_type = "lägenhet"
     MinRooms = "1"
-    MaxPrice = "3500000"
+    MaxPrice = "4000000"
     r = requests.get("https://api.booli.se/" +
                      query_type + "?q=" + areaId +
                      "&callerId=" + callerId +
@@ -37,7 +44,7 @@ def create_api_request(api_callerid, api_key, area):
                      "&objectType=" + object_type +
                      "&minRooms=" + MinRooms +
                      "&maxListPrice=" + MaxPrice)
-    print("Status code from API request = " + str(r.status_code) + "\n")
+    #  print("Status code from API request = " + str(r.status_code) + "\n")  #  Use for debug
     if r.status_code != 200:
         print(r.text)
         print('fail')
@@ -91,12 +98,12 @@ def create_html(df_html):
 
                 Hi,
                 <p>
-                    Here is your custom email notification regarding new listings. 
+                    Here is your custom email notification regarding new listings.
                     <b></b>
                     %s
                     <b></b>
                     Please contact rikard.fahlstrom@gmail.com if you have suggestions for improvements.
-                </p> 
+                </p>
              </body>
        </html>
    """ % (df_html)
@@ -118,14 +125,6 @@ def send_email(send_to, send_from_email, send_from_password, html):
     text = msg.as_string()
     server.sendmail(send_from_email, toaddr, text)
     server.quit()
-    print('Done')
-
-
-def main():
-    output = create_api_request('Rikard Fahlström', 'ENTER_API_KEY', '386690,870600')
-    df = create_df(output)
-    html = create_html(df)
-    send_email('rikard.fahlstrom@gmail.com', 'rikard.email.notification@gmail.com', 'SEND_EMAIL_PWD', html)
 
 
 if __name__ == '__main__':
